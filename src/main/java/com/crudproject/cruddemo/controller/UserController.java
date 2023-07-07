15409bcef883ccbea0f9c66432fc372c4c377910
apandagre <apandagre@argusoft.com>
2023-07-07 18:02:34 +0530

    user update & delete

--- src/main/java/com/crudproject/cruddemo/controller/UserController.java
+++ src/main/java/com/crudproject/cruddemo/controller/UserController.java
@@ -6,9 +6,7 @@ import com.crudproject.cruddemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -18,6 +16,7 @@ public class UserController {
 
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
+        System.out.println("here...");
         return userRepository.save(newUser);
     }
 
@@ -29,5 +28,25 @@ public class UserController {
     @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + "has been deleted successfully.";
     }
 }
--- src/main/java/com/crudproject/cruddemo/exception/UserNotFoundException.java
+++ src/main/java/com/crudproject/cruddemo/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.crudproject.cruddemo.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
-        super("No user exists with id" + id);
+        super("No user exists with id " + id);
     }
 }
